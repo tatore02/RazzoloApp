c5bfa3c8c037cdc57e2cf7ce56a4f5a29792b51a
Salvatore Ruocco <114869138+tatore02@users.noreply.github.com>
2023-11-04 22:25:14 +0100

    improve bfs

--- app/src/main/java/it/unisa/razzolo/Algorithms.java
+++ app/src/main/java/it/unisa/razzolo/Algorithms.java
@@ -1,25 +1,24 @@
 package it.unisa.razzolo;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 import it.unisa.razzolo.model.Point;
 
 public class Algorithms {
 
-    public static List<Point> bfs(char[][] matrix, int i, int j){
-        final boolean[][] isValid = new boolean[][]{{true,true,true,true},{true,true,true,true},{true,true,true,true},{true,true,true,true}};
-        final List<Point> l = new ArrayList<>();
+    public static boolean bfs(char[][] matrix, int i, int j, String word){
+        final boolean[][] isVisited = new boolean[4][4];
 
         final Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(i,j, matrix[i][j]));
-        isValid[i][j] = false;
+        queue.add(new Point(i,j, matrix[i][j], 0));
+        isVisited[i][j] = true;
 
         while(!queue.isEmpty()){
             final Point point = queue.poll();
-            l.add(point);
+            int currentIndex = point.getIndex();
+            if(currentIndex == word.length()-1)
+                return true;
 
             int x = point.getI();
             int y = point.getJ();
@@ -27,22 +26,23 @@ public class Algorithms {
             for(int k=0; k<8; k++){
                 int deltaX = x + dX[k];
                 int deltaY = y + dY[k];
-                if(checkCells(isValid, deltaX, deltaY)){
-                    queue.add(new Point(deltaY, deltaY, matrix[deltaX][deltaY]));
-                    isValid[deltaX][deltaX] = false;
+                if(checkCells(isVisited, deltaX, deltaY) && matrix[deltaX][deltaY] == word.charAt(currentIndex+1)){
+                    queue.add(new Point(deltaX, deltaY, matrix[deltaX][deltaY], currentIndex+1));
+                    isVisited[deltaX][deltaY] = true;
                 }
             }
         }
-        return l;
+        return false;
     }
 
     private static boolean checkCells(boolean[][] isValid, int i, int j){
-        if((i>=0 && i<=4) && (j>=0 && j<=4))
+        if((i>=0 && i<4) && (j>=0 && j<4))
             return isValid[i][j];
         return false;
     }
 
     // all possible directions
-    private static final int[] dX = { -1, 0, 1, 0, -1, 1, 1, -1};
-    private static final int[] dY = { 0, 1, 0, -1, -1, -1, 1, 1};
+    private static final int[] dX = { 0, 1, 1, 1, 0, -1, -1, -1};
+    private static final int[] dY = { -1, -1, 0, 1, 1, 1, 0, -1};
+                                    //up up-dx dx dw-dx dw dw-sx sx up-sx
 }
--- app/src/main/java/it/unisa/razzolo/model/Point.java
+++ app/src/main/java/it/unisa/razzolo/model/Point.java
@@ -4,11 +4,13 @@ public class Point {
     private final int i;
     private final int j;
     private final char value;
+    private final int index;
 
-    public Point(int i, int j, char value) {
+    public Point(int i, int j, char value, int index) {
         this.i = i;
         this.j = j;
         this.value = value;
+        this.index = index;
     }
 
     public int getI() {
@@ -21,5 +23,9 @@ public class Point {
 
     public char getValue() {
         return value;
+    }
+
+    public int getIndex() {
+        return index;
     }
 }
